863dd95c91350727a058628f6182b8d9fb24d728
FirentisTFW <jkb.pawel@gmail.com>
2023-03-15 21:45:22 +0100

    Update file structure

--- app/src/main/java/com/firentistfw/kindlehighlights/ui/main/MainActivity.kt
+++ app/src/main/java/com/firentistfw/kindlehighlights/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.firentistfw.kindlehighlights
+package com.firentistfw.kindlehighlights.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firentistfw.kindlehighlights.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
