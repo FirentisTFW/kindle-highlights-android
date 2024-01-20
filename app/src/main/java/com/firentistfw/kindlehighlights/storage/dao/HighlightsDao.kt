ad84dda538dfba1d9779e9b9620a76b0bbc4ab85
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-01-20 18:15:01 +0100

    Allow fetching highlights for categories and for books

--- app/src/main/java/com/firentistfw/kindlehighlights/storage/dao/HighlightsDao.kt
+++ app/src/main/java/com/firentistfw/kindlehighlights/storage/dao/HighlightsDao.kt
@@ -8,21 +8,38 @@ import androidx.room.Query
 import androidx.room.Transaction
 import com.firentistfw.kindlehighlights.storage.model.CompleteHighlight
 import com.firentistfw.kindlehighlights.storage.tables.DBHighlight
+import java.util.UUID
 
 @Dao
 interface HighlightsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun upsert(highlight: DBHighlight)
+    suspend fun upsert(highlight: DBHighlight)
 
     @Query("SELECT * FROM highlights")
-    fun getAll(): List<DBHighlight>
+    suspend fun getAll(): List<DBHighlight>
 
     @Transaction
     @Query("SELECT * FROM highlights")
     // FIXME Rename
-    fun getAllComplete(): List<CompleteHighlight>
+    suspend fun getAllComplete(): List<CompleteHighlight>
 
+    @Transaction
+    @Query(
+        "SELECT * FROM highlights " +
+                "INNER JOIN books on highlights.bookId = books.bookId " +
+                "WHERE books.bookId IN (:bookIds)"
+    )
+    suspend fun getForBooks(bookIds: List<UUID>): List<CompleteHighlight>
+
+    @Transaction
+    @Query(
+        "SELECT * FROM highlights " +
+                "INNER JOIN `highlight-category-cross-ref` crossRef on highlights.highlightId = crossRef.highlightId " +
+                "INNER JOIN categories on crossRef.categoryId = categories.categoryId " +
+                "   WHERE categories.categoryId IN (:categoryIds)"
+    )
+    suspend fun getForCategories(categoryIds: List<UUID>): List<CompleteHighlight>
 
     @Delete
-    fun delete(highlight: DBHighlight)
+    suspend fun delete(highlight: DBHighlight)
 }
