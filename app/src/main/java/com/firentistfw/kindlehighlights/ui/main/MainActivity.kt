82252c35b4f4893cc858985bee6c138641f69810
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-01-25 22:23:22 +0100

    Add basic options menu

--- app/src/main/java/com/firentistfw/kindlehighlights/ui/main/MainActivity.kt
+++ app/src/main/java/com/firentistfw/kindlehighlights/ui/main/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.firentistfw.kindlehighlights.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.firentistfw.kindlehighlights.R
 import com.firentistfw.kindlehighlights.common.BaseActivity
 import com.firentistfw.kindlehighlights.databinding.ActivityMainBinding
+import com.firentistfw.kindlehighlights.utils.ToastUtils
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
@@ -18,5 +21,23 @@ class MainActivity : BaseActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment, HighlightListFragment())
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            // TODO Implement navigation
+            R.id.miCategories -> ToastUtils.showFeatureUnavailable(this)
+            R.id.miAddCategory -> ToastUtils.showFeatureUnavailable(this)
+            R.id.miAllHighlights -> ToastUtils.showFeatureUnavailable(this)
+            R.id.miImportHighlights -> ToastUtils.showFeatureUnavailable(this)
+        }
+
+        return true
     }
 }
