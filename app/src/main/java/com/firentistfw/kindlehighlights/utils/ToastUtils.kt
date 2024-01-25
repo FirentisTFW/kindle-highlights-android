ead171e5ccd8fd2d45e49d5a17b2de3fb4f1f253
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-01-25 22:22:40 +0100

    Add content unavailable toast

--- app/src/main/java/com/firentistfw/kindlehighlights/utils/ToastUtils.kt
+++ app/src/main/java/com/firentistfw/kindlehighlights/utils/ToastUtils.kt
@@ -2,9 +2,14 @@ package com.firentistfw.kindlehighlights.utils
 
 import android.content.Context
 import android.widget.Toast
+import com.firentistfw.kindlehighlights.R
 
 object ToastUtils {
-    fun showSimpleToast(context: Context, text: String) {
+    fun showFeatureUnavailable(context: Context) {
+        showSimpleToast(context, context.getString(R.string.toastUtils_featureNotAvailable))
+    }
+
+    private fun showSimpleToast(context: Context, text: String) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 }
