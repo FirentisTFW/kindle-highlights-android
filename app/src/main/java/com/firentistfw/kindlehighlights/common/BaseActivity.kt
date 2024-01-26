97c4fa1e459e74834c4643317becf4b97ed1af59
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-01-26 21:48:34 +0100

    Update BaseActivity

--- app/src/main/java/com/firentistfw/kindlehighlights/common/BaseActivity.kt
+++ app/src/main/java/com/firentistfw/kindlehighlights/common/BaseActivity.kt
@@ -1,18 +1,7 @@
 package com.firentistfw.kindlehighlights.common
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-//        setContentView(layoutResId)
-        initInteractions()
-        initObservers()
-    }
-
     protected open fun initInteractions() {}
-
-    protected open fun initObservers() {}
 }
