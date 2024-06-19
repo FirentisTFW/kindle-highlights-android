77578082736a29ec311a687ff4ed3b6de05ac750
Paweł Jakubowski <jkb.pawel@gmail.com>
2024-06-19 22:29:22 +0200

    Add primary and danger button types

--- app/src/main/java/com/firentistfw/kindlehighlights/ui/common/KHButton.kt
+++ app/src/main/java/com/firentistfw/kindlehighlights/ui/common/KHButton.kt
@@ -9,13 +9,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.firentistfw.kindlehighlights.ui.styles.KHColors
 import com.firentistfw.kindlehighlights.ui.styles.KHTextStyles
 
+enum class KHButtonType {
+    Primary,
+    Danger;
+
+    val textColor: Color
+        get() = when (this) {
+            Primary -> KHColors.white
+            Danger -> KHColors.red
+        }
+
+    val colors: ButtonColors
+        get() = when (this) {
+            Primary -> ButtonColors(
+                containerColor = KHColors.teal,
+                contentColor = KHColors.white,
+                disabledContainerColor = KHColors.lightGrey,
+                disabledContentColor = KHColors.white,
+            )
+            Danger -> ButtonColors(
+                    containerColor = KHColors.white,
+                    contentColor = KHColors.red,
+                    disabledContainerColor = KHColors.lightGrey,
+                    disabledContentColor = KHColors.white,
+                )
+        }
+}
+
 @Composable
 fun KHButton(
     text: String,
+    type: KHButtonType = KHButtonType.Primary,
     enabled: Boolean = true,
     onClick: () -> Unit,
 ) {
@@ -29,12 +58,7 @@ fun KHButton(
             bottomEnd = ZeroCornerSize
         ),
         enabled = enabled,
-        colors = ButtonColors(
-            containerColor = KHColors.teal,
-            contentColor = KHColors.white,
-            disabledContainerColor = KHColors.lightGrey,
-            disabledContentColor = KHColors.white,
-        ),
+        colors = type.colors,
         contentPadding = PaddingValues(
             start = 16.dp,
             top = 16.dp,
@@ -42,6 +66,6 @@ fun KHButton(
             bottom = 16.dp,
         )
     ) {
-        Text(text.uppercase(), style = KHTextStyles.primaryButton)
+        Text(text.uppercase(), style = KHTextStyles.primaryButton, color = type.textColor)
     }
 }
